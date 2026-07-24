1f291b30e39cefe53829af6908078e817823dbac
Arun Kumar <arunsparrow096@gmail.com>
2026-07-24 09:48:44 +0530

    I have created linklist and empty list

---
+++ learning/emptylinkedlist.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+ 
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
+new_linked_list = LinkedList()
+print(new_linked_list.head)
+print(new_linked_list.tail)
+print(new_linked_list.length)
