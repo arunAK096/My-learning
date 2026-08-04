de59d3182e47d954a55e0774ebe5417fd31000f3
Arun Kumar <arunsparrow096@gmail.com>
2026-08-04 13:07:46 +0530

    have added some interview questions

---
+++ learning/Interview/removeduplicates.py
@@ -0,0 +1,21 @@
+from Nodeclass import LinkedList
+
+
+def remove_duplicates(ll: LinkedList):
+        if ll.head is None:
+            return
+        current_node = ll.head
+        prev_node = None
+        while current_node:
+            runner = current_node
+            while runner.next:
+                if runner.next.value == current_node.value:
+                    runner.next = runner.next.next
+                else:
+                    runner = runner.next
+            prev_node = current_node
+            current_node = current_node.next
+        ll.tail = prev_node
+        return ll.head
+        
+        
